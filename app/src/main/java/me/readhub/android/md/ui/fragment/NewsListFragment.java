e73faa92ee3fb3a29760acccd9fb31d639859813
TakWolf <takwolf@foxmail.com>
2017-06-13 16:45:26 +0800

    导航菜单

--- app/src/main/java/me/readhub/android/md/ui/fragment/NewsListFragment.java
+++ app/src/main/java/me/readhub/android/md/ui/fragment/NewsListFragment.java
@@ -1,7 +1,18 @@
 package me.readhub.android.md.ui.fragment;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.takwolf.android.hfrecyclerview.HeaderAndFooterRecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.readhub.android.md.R;
 import me.readhub.android.md.ui.base.TitledFragment;
 
 public class NewsListFragment extends TitledFragment {
@@ -9,6 +20,27 @@ public class NewsListFragment extends TitledFragment {
     @NonNull
     public static NewsListFragment newInstance() {
         return new NewsListFragment();
+    }
+
+    @BindView(R.id.refresh_layout)
+    SwipeRefreshLayout refreshLayout;
+
+    @BindView(R.id.recycler_view)
+    HeaderAndFooterRecyclerView recyclerView;
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
     }
 
     @Override
--- app/src/main/java/me/readhub/android/md/ui/fragment/TopicListFragment.java
+++ app/src/main/java/me/readhub/android/md/ui/fragment/TopicListFragment.java
@@ -1,7 +1,17 @@
 package me.readhub.android.md.ui.fragment;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.takwolf.android.hfrecyclerview.HeaderAndFooterRecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.readhub.android.md.R;
 import me.readhub.android.md.ui.base.TitledFragment;
 
 public class TopicListFragment extends TitledFragment {
@@ -11,6 +21,26 @@ public class TopicListFragment extends TitledFragment {
         return new NewsListFragment();
     }
 
+    @BindView(R.id.refresh_layout)
+    SwipeRefreshLayout refreshLayout;
+
+    @BindView(R.id.recycler_view)
+    HeaderAndFooterRecyclerView recyclerView;
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_topic_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
 
     @Override
     public int getPageTitleId() {
