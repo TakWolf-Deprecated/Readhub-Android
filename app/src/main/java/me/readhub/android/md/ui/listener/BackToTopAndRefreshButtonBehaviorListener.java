3cbc84163d5ea461eeac422733153d2e304a33f6
TakWolf <takwolf@foxmail.com>
2017-10-20 22:17:43 +0800

    返回按钮添加动画

--- app/src/main/java/me/readhub/android/md/ui/listener/BackToTopAndRefreshButtonBehaviorListener.java
+++ app/src/main/java/me/readhub/android/md/ui/listener/BackToTopAndRefreshButtonBehaviorListener.java
@@ -1,9 +1,15 @@
 package me.readhub.android.md.ui.listener;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.annotation.NonNull;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.Interpolator;
 
 public final class BackToTopAndRefreshButtonBehaviorListener {
 
@@ -11,42 +17,97 @@ public final class BackToTopAndRefreshButtonBehaviorListener {
 
     public static class ForRecyclerView extends RecyclerView.OnScrollListener {
 
-        private final View btnBackToTopAndRefresh;
+        private static final int SHOW_HIDE_ANIM_DURATION = 200;
 
-        public ForRecyclerView(@NonNull View btnBackToTopAndRefresh) {
-            this.btnBackToTopAndRefresh = btnBackToTopAndRefresh;
-            btnBackToTopAndRefresh.setVisibility(View.GONE);
+        private static final Interpolator SHOW_ANIM_INTERPOLATOR = new LinearOutSlowInInterpolator();
+        private static final Interpolator HIDE_ANIM_INTERPOLATOR = new FastOutLinearInInterpolator();
+
+        private static final int ANIM_STATE_SHOWING = 0;
+        private static final int ANIM_STATE_SHOWN = 1;
+        private static final int ANIM_STATE_HIDING = 2;
+        private static final int ANIM_STATE_HIDDEN = 3;
+
+        private final View button;
+        private final float movingDistance;
+        private int animState = ANIM_STATE_HIDDEN;
+
+        public ForRecyclerView(@NonNull View button) {
+            this.button = button;
+            movingDistance = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 64.0f, button.getResources().getDisplayMetrics());
+            button.setTranslationY(-movingDistance);
         }
 
         @Override
         public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-           handleBtnState(recyclerView);
+            updateState(recyclerView);
         }
 
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            handleBtnState(recyclerView);
+            updateState(recyclerView);
         }
 
-        private void handleBtnState(@NonNull RecyclerView recyclerView) {
+        private void updateState(@NonNull RecyclerView recyclerView) {
             RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
             if (layoutManager instanceof LinearLayoutManager) {
                 if (((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition() == 0) {
-                    hideBtn();
+                    hide();
                 } else {
-                    showBtn();
+                    show();
                 }
             } else {
-                hideBtn();
+                hide();
             }
         }
 
-        private void showBtn() {
-            btnBackToTopAndRefresh.setVisibility(View.VISIBLE);
+        private boolean isOrWillBeShown() {
+            return animState == ANIM_STATE_SHOWING || animState == ANIM_STATE_SHOWN;
         }
 
-        private void hideBtn() {
-            btnBackToTopAndRefresh.setVisibility(View.GONE);
+        private boolean isOrWillBeHidden() {
+            return animState == ANIM_STATE_HIDING || animState == ANIM_STATE_HIDDEN;
+        }
+
+        private void show() {
+            if (isOrWillBeShown()) {
+                return;
+            }
+            button.animate().cancel();
+            animState = ANIM_STATE_SHOWING;
+            button.animate()
+                    .alpha(1.0f)
+                    .translationY(0.0f)
+                    .setDuration(SHOW_HIDE_ANIM_DURATION)
+                    .setInterpolator(SHOW_ANIM_INTERPOLATOR)
+                    .setListener(new AnimatorListenerAdapter() {
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            animState = ANIM_STATE_SHOWN;
+                        }
+
+                    });
+        }
+
+        private void hide() {
+            if (isOrWillBeHidden()) {
+                return;
+            }
+            button.animate().cancel();
+            animState = ANIM_STATE_HIDING;
+            button.animate()
+                    .alpha(0.0f)
+                    .translationY(-movingDistance)
+                    .setDuration(SHOW_HIDE_ANIM_DURATION)
+                    .setInterpolator(HIDE_ANIM_INTERPOLATOR)
+                    .setListener(new AnimatorListenerAdapter() {
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            animState = ANIM_STATE_HIDDEN;
+                        }
+
+                    });
         }
 
     }
