3cced09c35f3c33e24b69c8387326a89f4ac855a
TakWolf <takwolf@foxmail.com>
2017-10-22 01:40:22 +0800

    支持 custom tabs

--- app/src/main/java/me/readhub/android/md/ui/util/Navigator.java
+++ app/src/main/java/me/readhub/android/md/ui/util/Navigator.java
@@ -5,23 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.customtabs.CustomTabsIntent;
 
 import me.readhub.android.md.R;
 import me.readhub.android.md.model.storage.shared.SettingShared;
 import me.readhub.android.md.ui.activity.ArticleActivity;
+import me.readhub.android.md.util.ResUtils;
 
 public final class Navigator {
 
     private Navigator() {}
 
     public static void openInBrowser(@NonNull Context context, @NonNull String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        if (intent.resolveActivity(context.getPackageManager()) != null) {
-            context.startActivity(intent);
-        } else {
-            ToastUtils.with(context).show(R.string.no_browser_install_in_system);
-        }
+        new CustomTabsIntent.Builder()
+                .setToolbarColor(ResUtils.getThemeAttrColor(context, R.attr.colorPrimary))
+                .build()
+                .launchUrl(context, Uri.parse(url));
     }
 
     public static void openShare(@NonNull Context context, @NonNull String text) {
