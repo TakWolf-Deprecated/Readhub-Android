49951d3de74491a60458cde39d054adbea25dd13
TakWolf <takwolf@foxmail.com>
2018-06-13 17:05:50 +0800

    修复注解

--- app/src/main/java/me/readhub/android/md/ui/util/ActivityUtils.java
+++ app/src/main/java/me/readhub/android/md/ui/util/ActivityUtils.java
@@ -1,12 +1,13 @@
 package me.readhub.android.md.ui.util;
 
 import android.app.Activity;
+import android.support.annotation.Nullable;
 
 public final class ActivityUtils {
 
     private ActivityUtils() {}
 
-    public static boolean isAlive(Activity activity) {
+    public static boolean isAlive(@Nullable Activity activity) {
         return activity != null && !activity.isDestroyed() && !activity.isFinishing();
     }
 
